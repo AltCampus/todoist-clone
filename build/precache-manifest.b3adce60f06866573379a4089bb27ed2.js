ee6c81504155859e7308e34d6cffed9ec88721e8
Sanjib Roy <sanjibroy0098@gmail.com>
2020-10-07 00:16:43 +0530

    Start implementing edit task

---
+++ build/precache-manifest.b3adce60f06866573379a4089bb27ed2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4720ee0fecd890a1b2ab013314a43e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e59394036af285dadec",
+    "url": "/static/css/main.149fad56.chunk.css"
+  },
+  {
+    "revision": "8faecc1bd670b0a67724",
+    "url": "/static/js/2.1b0ddf64.chunk.js"
+  },
+  {
+    "revision": "650bc0ff255588f6990bdcc253f0b934",
+    "url": "/static/js/2.1b0ddf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e59394036af285dadec",
+    "url": "/static/js/main.a0847994.chunk.js"
+  },
+  {
+    "revision": "1ed5b53a246752becdd8",
+    "url": "/static/js/runtime-main.aac46075.js"
+  },
+  {
+    "revision": "ca32d7d20348e72344be3e321ab55b27",
+    "url": "/static/media/logo.ca32d7d2.svg"
+  },
+  {
+    "revision": "9a223c19ca035f29d4f6e154490df3c7",
+    "url": "/static/media/logoSmall.9a223c19.svg"
+  },
+  {
+    "revision": "68ab164581bbc35d1865266b1fe25e7a",
+    "url": "/static/media/todoist-login.68ab1645.webp"
+  },
+  {
+    "revision": "c3df4b643eaba3ad42838c802346aaf5",
+    "url": "/static/media/todoistLogoBig.c3df4b64.svg"
+  }
+]);
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,7 +3,7 @@ import { ALL_PERSONAL_TASKS } from "../queries/index";
 import { useQuery } from "@apollo/react-hooks";
 import Loader from "./Loader";
 import { TodoContext } from "../App";
-import {TOGGLE_TASK_COMPLETION} from "../queries/index";
+import { TOGGLE_TASK_COMPLETION } from "../queries/index";
 import {
   FETCH_ALL_PERSONAL_TASK,
   MARK_AS_COMPLETED,
@@ -12,25 +12,28 @@ import { useMutation } from "@apollo/react-hooks";
 
 function Todo({ insertedData }) {
   const [updatedTask, setupdatedTask] = useState("");
+  const [editTask, setEditTask] = useState("");
 
   const [iconVisibility, setVisibility] = useState("");
   const context = useContext(TodoContext); // Contetx
   var { loading, error, data } = useQuery(ALL_PERSONAL_TASKS);
-  
-  const [toggleIsCompleted, { updateloading, updateerror }, updatedData] = useMutation(TOGGLE_TASK_COMPLETION, {
+
+  const [
+    toggleIsCompleted,
+    { updateloading, updateerror },
+    updatedData,
+  ] = useMutation(TOGGLE_TASK_COMPLETION, {
     update: (
-          proxy,
-          {
-            data: {
-              update_tasks: { returning },
-            },
-          }
-        ) => {
-          setupdatedTask(returning[0].task_id)
+      proxy,
+      {
+        data: {
+          update_tasks: { returning },
         },
-      
+      }
+    ) => {
+      setupdatedTask(returning[0].task_id);
+    },
   });
-  
 
   useEffect(() => {
     if (data && !loading) {
@@ -42,10 +45,10 @@ function Todo({ insertedData }) {
     toggleIsCompleted({
       variables: {
         is_completed: !is_completed,
-        task_id:taskId
+        task_id: taskId,
       },
     });
-    return context.dispatch({type: MARK_AS_COMPLETED, payload: taskId})
+    return context.dispatch({ type: MARK_AS_COMPLETED, payload: taskId });
   };
 
   if (loading || updateloading) {
@@ -56,46 +59,62 @@ function Todo({ insertedData }) {
     <div className="w-full">
       {context.state.personal_tasks &&
         context.state.personal_tasks.map((task) => {
-          if(task.is_completed) {
-            return <></>
+          if (task.is_completed) {
+            return <></>;
           }
           return (
-            <div
-              key={`task${task.task_id}`}
-              className="text-sm py-4 flex justify-between items-center"
-              onMouseEnter={() => setVisibility(task.task_id)}
-              onMouseLeave={() => setVisibility(task.task_id)}
-            >
-              {task.task_id &&
-              <>
-              <div className="inline-flex items-center">
-                <div onClick={() => handleCheck(task.task_id, task.is_completed)}>
-                  {task.is_completed ? (
-                    <i className="fas fa-check-circle cursor-pointer"></i>
-                  ) : (
-                    <i className="far fa-circle cursor-pointer"></i>
-                  )}
-                  <span className="ml-2 cursor-pointer">{task.title}</span>
-                </div>
-              </div>
-              
-              <div
-                className={
-                  iconVisibility === task.task_id
-                    ? `w-2/12 visible`
-                    : `invisible`
-                }
-              >
-                <ul className="flex justify-evenly items-center w-full">
-                  <li>
-                    <i className="far fa-comment-alt" />
-                  </li>
-                  <li>
-                    <i className="fas fa-pencil-alt" />
-                  </li>
-                </ul>
-              </div></>}
-            </div>
+            <>
+              {task.task_id !== editTask ? (
+                <>
+                  <div
+                    key={`task${task.task_id}`}
+                    className="text-sm py-4 flex justify-between items-center"
+                    onMouseEnter={() => setVisibility(task.task_id)}
+                    onMouseLeave={() => setVisibility(task.task_id)}
+                  >
+                    {task.task_id && (
+                      <>
+                        <div className="inline-flex items-center">
+                          <div
+                            onClick={() =>
+                              handleCheck(task.task_id, task.is_completed)
+                            }
+                          >
+                            {task.is_completed ? (
+                              <i className="fas fa-check-circle cursor-pointer"></i>
+                            ) : (
+                              <i className="far fa-circle cursor-pointer"></i>
+                            )}
+                            <span className="ml-2 cursor-pointer">
+                              {task.title}
+                            </span>
+                          </div>
+                        </div>
+
+                        <div
+                          className={
+                            iconVisibility === task.task_id
+                              ? `w-2/12 visible`
+                              : `invisible`
+                          }
+                        >
+                          <ul className="flex justify-evenly items-center w-full">
+                            <li>
+                              <i className="far fa-comment-alt" />
+                            </li>
+                            <li onClick={() =>setEditTask(task.task_id)}>
+                              <i className="fas fa-pencil-alt" />
+                            </li>
+                          </ul>
+                        </div>
+                      </>
+                    )}
+                  </div>
+                </>
+              ) : (
+                <>Render EditTask component</>
+              )}
+            </>
           );
         })}
     </div>
